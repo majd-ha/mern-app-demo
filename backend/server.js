03fc610ec8a71134b156cafea1b0ff8ea420971d
majd-ha <zienjm@gmail.com>
2023-04-06 16:12:56 +0300

    fix img

--- backend/server.js
+++ backend/server.js
@@ -16,6 +16,7 @@ app.use(
   })
 );
 app.use(express.json());
+app.use(express.static(__dirname + "/uploads"));
 // app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
