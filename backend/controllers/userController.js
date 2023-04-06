bb652f6e4cac1c041b8d9556ad287dc4cb50e0c3
majd-ha <zienjm@gmail.com>
2023-04-06 17:06:10 +0300

    try3

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -46,9 +46,7 @@ module.exports = {
       const hash = await bcrypt.hash(password, salt);
       const user = await User.create({
         email,
-        avatar: fs.readFileSync(
-          `/opt/render/project/src/backend/uploads\\a302008d965086ab4e869cde72d47721`
-        ),
+        avatar: fs.readFileSync(`uploads_a302008d965086ab4e869cde72d47721`),
         password: hash,
         phone,
         user_name,
